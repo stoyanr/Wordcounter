b236a3b32c20aee089505dd779879cbfba88e0a3
Stoyan Rachev <stoyanr@gmail.com>
2012-11-23 23:30:15 +0200

    Added more functionality and tests for WordCounter

--- wordfinder/src/main/java/com/stoyanr/wordfinder/WordCounter.java
+++ wordfinder/src/main/java/com/stoyanr/wordfinder/WordCounter.java
@@ -5,30 +5,50 @@
 package com.stoyanr.wordfinder;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 /**
- *
+ * 
  * @author i027947
  */
 class WordCounter {
 
     public Map<String, Integer> countWords(String text) {
+        if (text == null)
+            throw new IllegalArgumentException();
         Scanner scanner = new Scanner(text);
-        System.out.printf("Counting words in %s\n", text);
-        return count(scanner);
+        return countWords(scanner);
     }
 
-    public Map<String, Integer> countWords(File file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
-        System.out.printf("Counting words in %s\n", file);
-        return count(scanner);
+    public Map<String, Integer> countWords(File file) throws IOException {
+        if (file == null || !file.exists())
+            throw new IllegalArgumentException();
+        return (file.isDirectory()) ? countWordsDir(file) : countWords(new Scanner(file));
     }
 
-    private Map<String, Integer> count(Scanner scanner) {
+    private Map<String, Integer> countWordsDir(File dir) throws IOException {
+        Path path = Paths.get(dir.getPath());
+        WordCounterVisitor visitor = new WordCounterVisitor();
+        Files.walkFileTree(path, EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE,
+            visitor);
+        return visitor.getCounts();
+    }
+
+    public Map<String, Integer> countWords(Scanner scanner) {
+        if (scanner == null)
+            throw new IllegalArgumentException();
         Map<String, Integer> map = new HashMap<>();
         while (scanner.hasNext()) {
             String word = scanner.next();
@@ -37,4 +57,46 @@ class WordCounter {
         }
         return map;
     }
+
+    private class WordCounterVisitor implements FileVisitor<Path> {
+
+        private Map<String, Integer> counts = new HashMap<>();
+
+        public Map<String, Integer> getCounts() {
+            return counts;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+            throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            addCounts(counts, countWords(new Scanner(file.toFile())));
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
+
+    static void addCounts(Map<String, Integer> counts, Map<String, Integer> countsx) {
+        for (Entry<String, Integer> e : countsx.entrySet()) {
+            String word = e.getKey();
+            int count = (counts.containsKey(word)) ? counts.get(word) + e.getValue() : e
+                .getValue();
+            counts.put(word, count);
+        }
+    }
+
 }
--- wordfinder/src/test/java/com/stoyanr/wordfinder/WordCounterTest.java
+++ wordfinder/src/test/java/com/stoyanr/wordfinder/WordCounterTest.java
@@ -1,13 +1,17 @@
 package com.stoyanr.wordfinder;
 
-import java.io.File;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
 import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -19,42 +23,108 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class WordCounterTest {
 
-    private static final String TEXT = "one two three one two one";
-    private static final Map<String, Integer> WORDCOUNTS = new HashMap<>();
+    private static final String TEXT1 = "one two three one two one";
+    private static final String TEXT2 = "five six\tseven123\nfive";
+    private static final Map<String, Integer> COUNTS1 = new HashMap<>();
+    private static final Map<String, Integer> COUNTS2 = new HashMap<>();
+
+    private static final String DIR = "words";
+    private static final String FILE = "words.txt";
 
     static {
-        WORDCOUNTS.put("one", 3);
-        WORDCOUNTS.put("two", 2);
-        WORDCOUNTS.put("three", 1);
+        // @formatter:off
+        COUNTS1.put("one", 3); COUNTS1.put("two", 2); COUNTS1.put("three", 1);
+        COUNTS2.put("five", 2); COUNTS2.put("six", 1); COUNTS2.put("seven123", 1);
+        // @formatter:on
     }
 
     @Parameters
     public static Collection<Object[]> data() {
-        Object[][] data = new Object[][]{{TEXT, WORDCOUNTS}};
-        return Arrays.asList(data);
+        // @formatter:off
+        Object[][] data = new Object[][] { 
+            { asList(TEXT1), asList(COUNTS1) }, 
+            { asList(TEXT2), asList(COUNTS2) }, 
+            { asList(TEXT1, TEXT2), asList(COUNTS1, COUNTS2) }, 
+        };
+        // @formatter:on
+        return asList(data);
     }
-    private final String text;
-    private final Map<String, Integer> wordcounts;
 
-    public WordCounterTest(String text, Map<String, Integer> wordcounts) {
-        this.text = text;
-        this.wordcounts = wordcounts;
+    private final List<String> texts;
+    private final List<Map<String, Integer>> countsList;
+
+    private WordCounter counter;
+
+    public WordCounterTest(List<String> texts, List<Map<String, Integer>> countsList) {
+        this.texts = texts;
+        this.countsList = countsList;
+    }
+
+    @Before
+    public void setUp() {
+        counter = new WordCounter();
     }
 
     @Test
     public void testCountWordsString() {
-        WordCounter counter = new WordCounter();
-        Map<String, Integer> result = counter.countWords(text);
-        assertEquals(wordcounts, result);
+        Map<String, Integer> result = counter.countWords(text(texts));
+        assertEquals(combine(countsList), result);
     }
-    
+
     @Test
     public void testCountWordsFile() throws Exception {
-        File file = new File("words.txt");
-        FileUtils.writeStringToFile(file, text);
-        WordCounter counter = new WordCounter();
-        Map<String, Integer> result = counter.countWords(file);
-        assertEquals(wordcounts, result);
+        Map<String, Integer> result = counter.countWords(file(texts));
+        assertEquals(combine(countsList), result);
     }
-    
+
+    @Test
+    public void testCountWordsDirectoryTree() throws Exception {
+        Map<String, Integer> result = counter.countWords(tree(texts));
+        assertEquals(combine(countsList), result);
+    }
+
+    private static String text(List<String> texts) {
+        StringBuilder s = new StringBuilder();
+        for (String text : texts) {
+            s.append(text + "\n");
+        }
+        return s.toString();
+    }
+
+    private static File file(List<String> texts) throws IOException {
+        File file = new File(FILE);
+        FileUtils.writeStringToFile(file, text(texts));
+        return file;
+    }
+
+    private static File tree(List<String> texts) throws IOException {
+        File dir = new File(DIR);
+        deleteDir(dir);
+        dir.mkdirs();
+        int count = 0;
+        for (String text : texts) {
+            File dirx = new File(DIR + "/" + count);
+            dirx.mkdirs();
+            FileUtils.writeStringToFile(new File(DIR + "/" + count + "/" + FILE), text);
+            count++;
+        }
+        return dir;
+    }
+
+    private static void deleteDir(File dir) {
+        while (dir.exists()) {
+            try {
+                FileUtils.deleteDirectory(dir);
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private static Map<String, Integer> combine(List<Map<String, Integer>> countsList) {
+        Map<String, Integer> result = new HashMap<>();
+        for (Map<String, Integer> counts : countsList) {
+            WordCounter.addCounts(result, counts);
+        }
+        return result;
+    }
 }
